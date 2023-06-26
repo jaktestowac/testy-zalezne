32f63160a653614d6c10505dfa8315c977775396
Krzysiek <kkijas@jaktestowac.pl>
2023-06-26 16:04:39 +0200

    feat: add createArticleApi

--- tests/part7-api/ex1-api.spec.ts
+++ tests/part7-api/ex1-api.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test';
+import { APIRequestContext } from 'playwright-core';
 
 test.describe('creating article @p7-ex1', () => {
   let articleUrl = '';
@@ -34,7 +35,7 @@ test.describe('creating article @p7-ex1', () => {
   });
 });
 
-async function createArticle(page, articleTitle: string, articleBody: string) {
+async function createArticle(page, articleTitle: string, articleBody: string):Promise<void> {
   await page.getByTestId('open-articles').click();
   await page.getByRole('button', { name: 'Add Article' }).click();
 
@@ -43,7 +44,7 @@ async function createArticle(page, articleTitle: string, articleBody: string) {
   await page.getByTestId('save').click();
 }
 
-async function loginToService(page) {
+async function loginToService(page):Promise<void> {
   await page.goto('');
   await page.getByTestId('btn-dropdown').click();
   await page.getByRole('link', { name: 'Login' }).click();
@@ -52,3 +53,42 @@ async function loginToService(page) {
   await page.getByPlaceholder('Enter Password').fill('test1');
   await page.getByPlaceholder('Enter Password').press('Enter');
 }
+
+async function createArticleApi(request, articleTitle: string, articleBody: string) {
+  const newArticleData = {
+        user_id: 1,
+        title: articleTitle,
+        body: articleBody,
+        date: '2023-05-26T11:21:00Z',
+        image: '.\\data\\images\\256\\chuttersnap-9cCeS9Sg6nU-unsplash.jpg',
+    };
+
+    const token = await loginAndReturnToken(request);
+    const loggedHeaders = {
+        Authorization: `Bearer ${token}`,
+    };
+
+    // Act
+    const createArticleResponse = await request.post(endpoints.articles, {
+        data: newArticleData,
+        headers: loggedHeaders,
+    });
+}
+
+export async function loginAndReturnToken(request: APIRequestContext): Promise<string> {
+    const loginData = {
+      email: 'Moses.Armstrong@Feest.ca',
+      password: "test1"
+    }
+
+    // login:
+    const loginResponse = await request.post(endpoints.login, {data: loginData});
+    const loginResponseData = await loginResponse.json();
+
+    return loginResponseData.access_token;
+}
+
+export const endpoints = {
+    articles: '/api/articles/',
+    login: '/api/login/'
+};
